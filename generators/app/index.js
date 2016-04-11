d8bd0c2174ea92f1228d6b4eb57abc3f908aba25
Mikami Hiroaki <hiroaki8270@gmail.com>
2016-04-12 08:38:49 +0900

    Fix the default name of the document
      The default name should not contain white-spaces.
      ref #2

--- generators/app/index.js
+++ generators/app/index.js
@@ -14,7 +14,7 @@ module.exports = yeoman.Base.extend({
         type: 'input',
         name: 'name',
         message: 'Your document name',
-        default: this.appname
+        default: this.appname.split(" ").join("-")
       },
       {
         type: 'list',
