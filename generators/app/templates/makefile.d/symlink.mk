c37d55fdc830601614190b75002ae13b6e55f4cf
Mikami Hiroaki <hiroaki8270@gmail.com>
2016-04-15 20:30:23 +0900

    Remove use of realpath command
      ref #12

--- generators/app/templates/makefile.d/symlink.mk
+++ generators/app/templates/makefile.d/symlink.mk
@@ -2,35 +2,35 @@
 ## Tex files
 $(generatedMainTexFile): $(mainTexFile)
 	mkdir -p $(generated)
-	if [ ! -n "`find $@ -maxdepth 0 -type l 2> /dev/null`" ]; then \
-	  ln -s `realpath $< --relative-to=$(shell dirname $@)` $@; \
+	if [ -z "$(realpath $@)" ]; then \
+		ln -s $(abspath $<) $@; \
 	fi
 $(generatedDoc)/%.tex: $(doc)/%.tex
 	mkdir -p $(generatedDoc)
-	if [ ! -n "`find $@ -maxdepth 0 -type l 2> /dev/null`" ]; then \
-	  ln -s `realpath $< --relative-to=$(shell dirname $@)` $@; \
+	if [ -z "$(realpath $@)" ]; then \
+		ln -s $(abspath $<) $@; \
 	fi
 
 ## Image files
 $(generatedImage)/%.png: $(image)/%.png
 	mkdir -p $(generatedImage)
-	if [ ! -n "`find $@ -maxdepth 0 -type l 2> /dev/null`" ]; then \
-	  ln -s `realpath $< --relative-to=$(shell dirname $@)` $@; \
+	if [ -z "$(realpath $@)" ]; then \
+		ln -s $(abspath $<) $@; \
 	fi
 $(generatedImage)/%.jpg: $(image)/%.jpg
 	mkdir -p $(generatedImage)
-	if [ ! -n "`find $@ -maxdepth 0 -type l 2> /dev/null`" ]; then \
-	  ln -s `realpath $< --relative-to=$(shell dirname $@)` $@; \
+	if [ -z "$(realpath $@)" ]; then \
+		ln -s $(abspath $<) $@; \
 	fi
 $(generatedImage)/%.pdf: $(image)/%.pdf
 	mkdir -p $(generatedImage)
-	if [ ! -n "`find $@ -maxdepth 0 -type l 2> /dev/null`" ]; then \
-	  ln -s `realpath $< --relative-to=$(shell dirname $@)` $@; \
+	if [ -z "$(realpath $@)" ]; then \
+		ln -s $(abspath $<) $@; \
 	fi
 
 ## BibTex files
 $(generatedBib)/%.bib: $(bib)/%.bib
 	mkdir -p $(generatedBib)
-	if [ ! -n "`find $@ -maxdepth 0 -type l 2> /dev/null`" ]; then \
-	  ln -s `realpath $< --relative-to=$(shell dirname $@)` $@; \
+	if [ -z "$(realpath $@)" ]; then \
+		ln -s $(abspath $<) $@; \
 	fi
